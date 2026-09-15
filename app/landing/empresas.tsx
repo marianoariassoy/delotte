4506c5c8b5622242b324d87db561f8c409b8ad7b
marianoariassoy <hola@marianoarias.soy>
2026-09-15 12:21:36 -0300

    fix: image aspect ratio and loading optimization

--- app/landing/empresas.tsx
+++ app/landing/empresas.tsx
@@ -2,14 +2,42 @@ export default function Empresas() {
   return (
     <div className="pt-4 pb-40 md:pb-70 w-full">
       <h2 className="text-sm mb-8">NUESTRAS EMPRESAS</h2>
-      <div className="flex gap-4 flex-wrap justify-center items-center [&>img]:max-w-40 md:[&>img]:max-w-70 [&>img]:h-auto max-w-7xl mx-auto">
-        <img src="/images/Elementos-03.svg" alt="Grupo Delotte" />
-        <img src="/images/Elementos-04.svg" alt="Grupo Delotte" />
-        <img src="/images/Elementos-05.svg" alt="Grupo Delotte" />
-        <img src="/images/Elementos-06.svg" alt="Grupo Delotte" />
-        <img src="/images/Elementos-07.svg" alt="Grupo Delotte" />
-        <img src="/images/Elementos-08.svg" alt="Grupo Delotte" />
-        <img src="/images/Elementos-09.svg" alt="Grupo Delotte" />
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 justify-center items-center max-w-7xl mx-auto">
+        <img
+          src="/images/Elementos-03.svg"
+          alt="Grupo Delotte"
+          className="w-full col-span-2 md:col-span-1 h-auto"
+        />
+        <img
+          src="/images/Elementos-04.svg"
+          alt="Grupo Delotte"
+          className="w-full h-auto"
+        />
+        <img
+          src="/images/Elementos-05.svg"
+          alt="Grupo Delotte"
+          className="w-full h-auto"
+        />
+        <img
+          src="/images/Elementos-06.svg"
+          alt="Grupo Delotte"
+          className="w-full h-auto"
+        />
+        <img
+          src="/images/Elementos-07.svg"
+          alt="Grupo Delotte"
+          className="w-full h-auto"
+        />
+        <img
+          src="/images/Elementos-08.svg"
+          alt="Grupo Delotte"
+          className="w-full h-auto"
+        />
+        <img
+          src="/images/Elementos-09.svg"
+          alt="Grupo Delotte"
+          className="w-full h-auto"
+        />
       </div>
     </div>
   );
